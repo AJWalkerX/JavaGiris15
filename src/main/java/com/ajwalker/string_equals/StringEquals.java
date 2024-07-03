0c08b1ff2be2e3f0c5897029977c58613544e4e1
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-07-03 10:39:29 +0300

    First commit

---
+++ src/main/java/com/ajwalker/string_equals/StringEquals.java
@@ -0,0 +1,17 @@
+package com.ajwalker.string_equals;
+
+import java.util.Scanner;
+
+public class StringEquals {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = "alex";
+		System.out.println("kullanici adinizi giriniz: ");
+		String input = sc.nextLine();
+		if (name.equals(input)){// equalsIgnoreCase'de olur
+			System.out.println("Hosgeldiniz!");
+		}else{
+			System.out.println("Kullanici adi hatali!");
+		}
+	}
+}

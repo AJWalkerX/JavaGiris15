f0e8051e156d6878725bc5bb8df626ef8a8ad2dd
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-07-08 12:34:26 +0300

    week 03 day01 lesson codes added.

---
+++ src/main/java/com/ajwalker/week03/diziler/ArrayMaxAndMin.java
@@ -0,0 +1,19 @@
+package com.ajwalker.week03.diziler;
+/*
+	!Burayı tamamla
+	Kullanıcıdan dizinin boyutunu belirlemesini sağla.
+	Dizinin değerlerini kullanıcıdan iste.
+	Hangi indexin en buyuk olduğunu bul.
+	kaçıncı index olduğunu da yazsın.
+	
+	Aynısının min degerini de bulsun.
+*/
+import java.util.Scanner;
+
+public class ArrayMaxAndMin {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Dizi kac elemanli olsun?");
+		int numbers[] = new int[sc.nextInt()];
+	}
+}
